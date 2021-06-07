13ed468e56ffe1c6e687e582d33fb6394f266626
someusernameidkman <al18634@aemaia.com>
2021-06-07 12:40:10 +0100

    2.3

---
+++ ProjetoANPRI/Assets/Scripts/Apanhar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apanhar : MonoBehaviour
+{
+    public Collider Player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player")) {
+            transform.parent = other.transform;
+            Player = other;
+            Player.gameObject.tag = "PlayerComOvo";
+        } else if (other.gameObject.tag.Equals("Entrega")) {
+            transform.parent = other.transform;
+            Player.gameObject.tag = "Player";
+        }
+    }
+}
